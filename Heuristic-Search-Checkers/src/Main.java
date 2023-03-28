f091dea91cd2404a2080ed903ac43300fe6228d2
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2023-03-28 22:58:26 +0200

    Update .gitignore and heuristic function

--- Heuristic-Search-Checkers/src/Main.java
+++ Heuristic-Search-Checkers/src/Main.java
@@ -6,13 +6,46 @@ public class EvaluatePosition // This class is required - don't remove it
     static private boolean _color; // This field is required - don't remove it
 
     // Define point values for pieces
-    private static final int REGULAR_PIECE_VALUE = 1;
+    private static final int PAWN_VALUE = 1;
     private static final int KING_VALUE = 5;
-    private static final int CENTER_VALUE = 1;
 
-    static private int pieceOrKing(AIBoard board, int row, int column)
+    private static final int SAFE_PAWN_VALUE = 2;
+    private static final int SAFE_KING_VALUE = 3;
+
+    private static final int MOVABLE_PAWN_VALUE = 3;
+    private static final int MOVABLE_KING_VALUE = 4;
+
+    private static final int CENTER_VALUE = 5;
+
+    private static final int OCCUPIED_FIELD_ON_PROMOTION_LINE = 2;
+
+    private static final int DEFENDER_PIECE = 3;
+
+    static private int pawnOrKing(AIBoard board, int row, int column)
     {
-        return (board._board[row][column].king) ? KING_VALUE : REGULAR_PIECE_VALUE;
+        return (board._board[row][column].king) ? KING_VALUE : PAWN_VALUE;
+    }
+
+    static private int safePawnOrKing(AIBoard board, int row, int column)
+    {
+        int size=board.getSize();
+
+        if(row == 0 || row == size - 1 || column == size -1 || column == 7)
+        {
+            return (board._board[row][column].king) ? SAFE_KING_VALUE : SAFE_PAWN_VALUE;
+        }
+        return 0;
+    }
+
+    static private int movablePawnOrKingWithoutCapturing(AIBoard board, int row, int column)
+    {
+        int direction = (board._board[row][column].white==getColor()) ? 1 : -1;
+
+        if(board._board[row+direction][column-1].empty && board._board[row+direction][column+1].empty)
+        {
+            return (board._board[row][column].king) ? MOVABLE_KING_VALUE: MOVABLE_PAWN_VALUE;
+        }
+        return 0;
     }
 
     static private int pieceInCenter(int row, int columns)
@@ -26,12 +59,38 @@ public class EvaluatePosition // This class is required - don't remove it
 
         if(board._board[row][column].white==getColor())
         {
-            return (board._board[row][column].king) ? 0 : size - 1 - row;
+            return (board._board[row][column].king) ? 0 : row/2;
         }
         else
         {
-            return (board._board[row][column].king) ? 0 : row;
+            return (board._board[row][column].king) ? 0 : (size - 1 - row)/2;
         }
+    }
+
+    static private int occupiedFieldOnPromotionLine(AIBoard board, int row, int column)
+    {
+        if(board._board[row][column].white==getColor())
+        {
+            if(row == 0) return OCCUPIED_FIELD_ON_PROMOTION_LINE;
+        }
+        else
+        {
+            if(row == 7) return OCCUPIED_FIELD_ON_PROMOTION_LINE;
+        }
+        return 0;
+    }
+
+    static private int defenderPiece(AIBoard board, int row, int column)
+    {
+        if(board._board[row][column].white==getColor())
+        {
+            if(row == 0 || row == 1) return DEFENDER_PIECE;
+        }
+        else
+        {
+            if(row == 6 || row == 7) return DEFENDER_PIECE;
+        }
+        return 0;
     }
 
     static public void changeColor(boolean color) // This method is required - don't remove it
@@ -47,6 +106,7 @@ public class EvaluatePosition // This class is required - don't remove it
         int myRating=0;
         int opponentsRating=0;
         int size=board.getSize();
+
         for (int i=0;i<size;i++)
         {
             for (int j=(i+1)%2;j<size;j+=2)
@@ -55,19 +115,28 @@ public class EvaluatePosition // This class is required - don't remove it
                 {
                     if (board._board[i][j].white==getColor()) // this is my piece
                     {
-                        myRating += pieceOrKing(board,i,j);
+                        myRating += pawnOrKing(board,i,j);
+                        myRating += safePawnOrKing(board,i,j);
+                        myRating += movablePawnOrKingWithoutCapturing(board,i,j);
                         myRating += pieceInCenter(i,j);
-                        myRating +=  distanceToPromotion(board,i,j);
+                        myRating += distanceToPromotion(board,i,j);
+                        myRating += occupiedFieldOnPromotionLine(board,i,j);
+                        myRating += defenderPiece(board,i,j);
                     }
                     else
                     {
-                        opponentsRating += pieceOrKing(board, i ,j);
+                        opponentsRating += pawnOrKing(board, i ,j);
+                        opponentsRating += safePawnOrKing(board,i,j);
+                        opponentsRating += movablePawnOrKingWithoutCapturing(board,i,j);
                         opponentsRating += pieceInCenter(i,j);
                         opponentsRating += distanceToPromotion(board,i,j);
+                        opponentsRating += occupiedFieldOnPromotionLine(board,i,j);
+                        opponentsRating += defenderPiece(board,i,j);
                     }
                 }
             }
         }
+
         //Judge.updateLog("Type your message here, you will see it in the log window\n");
         if (myRating==0) return LOSE;
         else if (opponentsRating==0) return WIN;
