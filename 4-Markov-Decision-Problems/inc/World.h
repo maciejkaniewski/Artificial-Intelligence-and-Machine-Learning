5d5a898e9c6e5e2eeb9e46352d0ec91471268ba2
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2023-05-04 19:31:22 +0200

    Create World class

---
+++ 4-Markov-Decision-Problems/inc/World.h
@@ -0,0 +1,27 @@
+#ifndef INC_WORLD_H
+#define INC_WORLD_H
+
+#include <vector>
+#include <tuple>
+
+class World {
+
+public:
+
+    World() = default;
+
+private:
+    int width_x_ = 0; // Defines the horizontal world size
+    int height_y_ = 0; // Defines the vertical world size
+    int start_x_ = 0; // Specifies the horizontal coordinate of the start state
+    int start_y_ = 0; // Specifies the vertical coordinate of the start state
+    std::vector<float> p_ = {0, 0, 0}; // Uncertainty distribution
+    float reward_ = 0; // Default reward parameter
+    float gamma_ = 0; // Discounting parameter
+    float epsilon_ = 0; // Exploration parameter
+    std::vector<std::tuple<int, int, float>> terminal_states_ = {}; // Terminal states (X,Y) and their reward
+    std::vector<std::tuple<int, int, float>> special_states_ = {}; // Special states (X,Y) and their reward
+    std::vector<std::pair<int, int>> forbidden_states_ = {}; // Forbidden states (X,Y)
+};
+
+#endif //INC_WORLD_H
--- 4-Markov-Decision-Problems/src/main.cpp
+++ 4-Markov-Decision-Problems/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "World.h"
 
 int main()
 {
     std::cout<<"Project Template"<<std::endl;
+    World testWorld;
 }
