48e54593f97b558986dcc51d3ed39fb957bd97ac
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2023-05-12 11:53:45 +0200

    Add -plot command line option

--- 4-Markov-Decision-Problems/src/main.cpp
+++ 4-Markov-Decision-Problems/src/main.cpp
@@ -6,26 +6,31 @@ int main(int argc, char **argv) {
 
   World world;
 
-  if (argc > 1 && argc <= 4) {
+  if (argc > 1 && argc <= 5) {
 
     // Validate file and load world parameters
     if (!world.loadWorldParametersFromFile(argv[1])) return 1;
 
     // Parse gamma from command line argument
-    if (argc > 2 && !world.setGamma(std::stod(argv[2]))) return 1;
+    if (argc > 2 && std::string(argv[2]) != "-plot" && !world.setGamma(std::stod(argv[2]))) return 1;
 
     // Parse epsilon from command line argument
-    if (argc > 3) world.setEpsilon(std::stod(argv[3]));
+    if (argc > 3 && std::string(argv[3]) != "-plot") world.setEpsilon(std::stod(argv[3]));
 
     world.printWorldParameters();
     world.constructWorld();
     ValueIterationAlgorithm::start(world);
     world.displayWorld();
 
-    Plotter::plot(ValueIterationAlgorithm::saved_state_utilities_);
+    for (int i = 2; i < argc; i++) {
+      if (std::string(argv[i]) == "-plot") {
+        Plotter::plot(ValueIterationAlgorithm::saved_state_utilities_);
+        break;
+      }
+    }
 
   } else {
-    std::cerr << "Usage: " << argv[0] << " <file_name> [<gamma> <epsilon>]" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " <file_name> [<gamma>] [<epsilon>] [-plot]" << std::endl;
     return 1;
   }
 }
