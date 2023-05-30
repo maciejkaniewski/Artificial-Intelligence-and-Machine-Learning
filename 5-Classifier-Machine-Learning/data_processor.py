ee3ae3bdcd6ad353fa859085b88dd165cf7c5457
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2023-05-30 19:33:45 +0200

    Change ntlk download directory

--- 5-Classifier-Machine-Learning/data_processor.py
+++ 5-Classifier-Machine-Learning/data_processor.py
@@ -13,11 +13,12 @@ class DataProcessor:
         """
         DataProcessor constructor.
         """
-        nltk.download('punkt')
-        nltk.download('stopwords')
+        nltk.download('punkt', download_dir='ntlk_data')
+        nltk.download('stopwords', download_dir='ntlk_data')
+        nltk.data.path.append('ntlk_data')
         self.stop_words = set(stopwords.words('english'))
 
-    def preprocess_text(self, text):
+    def preprocess_text(self, text) -> str:
         """
         Performs preprocessing operations on single text record.
         :param text: Single text record to be preprocessed.
@@ -30,7 +31,7 @@ class DataProcessor:
         preprocessed_text = ' '.join(filtered_tokens)
         return preprocessed_text
 
-    def preprocess_data(self, data):
+    def preprocess_data(self, data) -> list[str]:
         """
         Performs preprocess_text() on whole dataset.
         :param data: Dataset to be preprocessed.
