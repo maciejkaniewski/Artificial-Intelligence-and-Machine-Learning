989912170e88fe55bcc5a6df0394a3c8d50869e1
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2023-05-30 22:02:07 +0200

    Introduce pipelines

--- 5-Classifier-Machine-Learning/main.py
+++ 5-Classifier-Machine-Learning/main.py
@@ -1,52 +1,87 @@
+from typing import List, Tuple, Any
+
 from sklearn.datasets import load_files
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.pipeline import Pipeline
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 from data_processor import DataProcessor
 
-# Load data
-data = load_files("data", encoding="latin-1")
 
-# Preprocess the loaded data
-data_preprocessor = DataProcessor()
-preprocessed_data = data_preprocessor.preprocess_data(data.data)
+def perform_text_classification(data_sets, text_clf_pipeline) -> list[tuple[list[Any], Any, float]]:
+    pipeline_methods = [step[1] for step in text_clf_pipeline.steps]
+    print("Classification Pipeline:", pipeline_methods)
 
-data_sets = {"Raw Dataset": data.data,
-             "Preprocessed Dataset": preprocessed_data}
+    # Store the results
+    results = []
 
-# Create a figure with two subplots
-fig, axes = plt.subplots(1, 2, figsize=(16, 6))
+    # Create a figure with two subplots
+    fig, axes = plt.subplots(1, 2, figsize=(16, 6))
 
-for i, (config_name, data_set) in enumerate(data_sets.items()):
-    # Split the data into training and testing sets (80% for training, 20% for testing)
-    x_train, x_test, y_train, y_test = train_test_split(data_set, data.target, test_size=0.2, random_state=42)
-    # Create a Bag-of-Words object
-    bag_of_words_tokenizer = CountVectorizer()
-    # Learn the vocabulary dictionary and transform the training data into feature vectors
-    x_train_bow = bag_of_words_tokenizer.fit_transform(x_train)
-    # Transform the testing data into feature vectors
-    x_test_bow = bag_of_words_tokenizer.transform(x_test)
-    # Create a Naive Bayes classifier
-    classifier = MultinomialNB()
-    # Train the classifier
-    classifier.fit(x_train_bow, y_train)
-    # Predict the labels for the test set
-    y_pred = classifier.predict(x_test_bow)
-    # Print accuracy and classification report
-    print(f"{config_name} Accuracy:", accuracy_score(y_test, y_pred))
-    print(f"{config_name} Classification Report:\n",
-          classification_report(y_test, y_pred, target_names=data.target_names))
-    # Plot confusion matrix
-    ax = axes[i]
-    sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt="d", cmap="Blues",
-                xticklabels=data.target_names, yticklabels=data.target_names, ax=ax)
-    ax.set_xlabel("Predicted")
-    ax.set_ylabel("True")
-    ax.set_title(f"{config_name} Confusion Matrix")
+    for i, (config_name, data_set) in enumerate(data_sets.items()):
+        # Split the data into training and testing sets (80% for training, 20% for testing)
+        x_train, x_test, y_train, y_test = train_test_split(data_set, data.target, test_size=0.2, random_state=42)
+        # Train the classifier
+        text_clf_pipeline.fit(x_train, y_train)
+        # Predict the labels for the test set
+        y_pred = text_clf_pipeline.predict(x_test)
+        # Print accuracy and classification report
+        results.append((pipeline_methods, config_name, accuracy_score(y_test, y_pred)))
+        print(f"{config_name} Accuracy:", accuracy_score(y_test, y_pred))
+        print(f"{config_name} Classification Report:\n",
+              classification_report(y_test, y_pred, target_names=data.target_names))
+        # Plot confusion matrix
+        sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt="d", cmap="Blues",
+                    xticklabels=data.target_names, yticklabels=data.target_names, ax=axes[i])
+        axes[i].set_xlabel("Predicted")
+        axes[i].set_ylabel("True")
+        axes[i].set_title(f"{config_name} Confusion Matrix")
 
-# Show the figure with both confusion matrices
-plt.show()
+    # Show the figure with both confusion matrices
+    plt.show()
+    return results
+
+
+if __name__ == "__main__":
+    # Load data
+    data = load_files("data", encoding="latin-1")
+
+    # Preprocess the loaded data
+    data_preprocessor = DataProcessor()
+    preprocessed_data = data_preprocessor.preprocess_data(data.data)
+
+    data_sets = {"Raw Dataset": data.data,
+                 "Preprocessed Dataset": preprocessed_data}
+
+    classification_results = []
+
+    classification_results.append(
+        perform_text_classification(
+            data_sets,
+            Pipeline(
+                [
+                    ('Bag_Of_Words', CountVectorizer()),
+                    ('Naive_Bayes', MultinomialNB()),
+                ],
+            )))
+
+    classification_results.append(
+        perform_text_classification(
+            data_sets,
+            Pipeline(
+                [
+                    ('Bigram', CountVectorizer(ngram_range=(2, 2))),
+                    ('Naive_Bayes', MultinomialNB()),
+                ],
+            )))
+
+    flattened_list = [item for sublist in classification_results for item in sublist]
+    best_result = max(flattened_list, key=lambda x: x[2])
+
+    print("Best Accuracy:", best_result[2])
+    print("Pipeline Methods:", best_result[0])
+    print("Dataset Type:", best_result[1])
