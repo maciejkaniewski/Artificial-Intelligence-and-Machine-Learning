ed2a8444c7c30235ed45613dbed44840176209a5
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2023-06-02 22:55:47 +0200

    Test new models, change script flow

--- 5-Classifier-Machine-Learning/main.py
+++ 5-Classifier-Machine-Learning/main.py
@@ -1,68 +1,106 @@
-from typing import Any
-
 from sklearn.datasets import load_files
-from sklearn.model_selection import cross_val_score
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 from tabulate import tabulate
-
-
-def perform_text_classification(data_set, text_clf_pipeline) -> list[tuple[list[Any], float]]:
-    pipeline_methods = [step[1] for step in text_clf_pipeline.steps]
-    print("Classification Pipeline:", pipeline_methods)
-    # Store the results
-    results = []
-    # Train the classifier using cross-validation
-    cv_scores = cross_val_score(text_clf_pipeline, data_set, data.target, cv=10)
-    # Print cross-validation scores
-    print("Mean Cross-Validation Score:", cv_scores.mean())
-    print("Cross-Validation Score Variance:", cv_scores.var())
-    print()
-    # Print accuracy and classification report
-    results.append((pipeline_methods, cv_scores.mean()))
-    return results
-
 
 if __name__ == "__main__":
 
-    # Load data
     data = load_files("data", encoding="latin-1")
 
-    classification_results = [perform_text_classification(
-        data.data,
-        Pipeline(
-            [
-                ('Bag_Of_Words', CountVectorizer(stop_words='english')),
-                ('Naive_Bayes', MultinomialNB()),
-            ],
-        )), perform_text_classification(
-        data.data,
-        Pipeline(
-            [
-                ('Bigram', CountVectorizer(ngram_range=(2, 2), stop_words='english')),
-                ('Naive_Bayes', MultinomialNB()),
-            ],
-        )), perform_text_classification(
-        data.data,
-        Pipeline(
-            [
-                ('TF-IDF', TfidfVectorizer(stop_words='english')),
-                ('Naive_Bayes', MultinomialNB()),
-            ],
-        ))]
+    param_grid_naive_bayes_cv = {
+        'Count_Vectorizer__stop_words': ['english', None],
+        'Count_Vectorizer__ngram_range': [(1, 1), (1, 2), (2, 2)],
+        'Naive_Bayes__alpha': [0.1, 1.0]
+    }
 
-    table_data = []
-    for outer_list in classification_results:
-        pipeline_methods = ', '.join(str(step) for step in outer_list[0][0])
-        accuracy_values = [entry[1] for entry in outer_list]
-        accuracy_values = [str(value) for value in accuracy_values]
-        table_data.append([pipeline_methods] + accuracy_values)
+    param_grid_naive_bayes_tfidf = {
+        'TF-IDF__stop_words': ['english', None],
+        'TF-IDF__ngram_range': [(1, 1), (1, 2), (2, 2)],
+        'Naive_Bayes__alpha': [0.1, 1.0]
+    }
 
-    print(tabulate(table_data, headers=['Classification Pipeline', 'Mean Cross-Validation Score'], tablefmt='grid'))
-    print()
+    param_grid_decision_tree = {
+        'Count_Vectorizer__stop_words': ['english', None],
+        'Count_Vectorizer__ngram_range': [(1, 1), (1, 2), (2, 2)],
+        'Decision_Tree__max_depth': [None, 5, 10]
+    }
 
-    best_result = max([item for sublist in classification_results for item in sublist], key=lambda x: x[1])
+    param_grid_knn = {
+        'Count_Vectorizer__stop_words': ['english', None],
+        'Count_Vectorizer__ngram_range': [(1, 1), (1, 2), (2, 2)],
+        'Nearest_Neighbors__n_neighbors': [3, 5, 7]
+    }
 
-    print(f"The best model is {best_result[0]} with {round(best_result[1] * 100, 2)}% "f"mean cross-validation score.")
+    param_grid_svm = {
+        'Count_Vectorizer__stop_words': ['english', None],
+        'Count_Vectorizer__ngram_range': [(1, 1), (1, 2), (2, 2)],
+        'SVM__C': [0.1, 1.0]
+    }
+
+    grid_search_naive_bayes_cv = GridSearchCV(
+        estimator=Pipeline([
+            ('Count_Vectorizer', CountVectorizer()),
+            ('Naive_Bayes', MultinomialNB())
+        ]),
+        param_grid=param_grid_naive_bayes_cv,
+        cv=10
+    )
+
+    grid_search_naive_bayes_tfidf = GridSearchCV(
+        estimator=Pipeline([
+            ('TF-IDF', TfidfVectorizer()),
+            ('Naive_Bayes', MultinomialNB())
+        ]),
+        param_grid=param_grid_naive_bayes_tfidf,
+        cv=10
+    )
+
+    grid_search_decision_tree = GridSearchCV(
+        estimator=Pipeline([
+            ('Count_Vectorizer', CountVectorizer()),
+            ('Decision_Tree', DecisionTreeClassifier())
+        ]),
+        param_grid=param_grid_decision_tree,
+        cv=10
+    )
+
+    grid_search_knn = GridSearchCV(
+        estimator=Pipeline([
+            ('Count_Vectorizer', CountVectorizer()),
+            ('Nearest_Neighbors', KNeighborsClassifier())
+        ]),
+        param_grid=param_grid_knn,
+        cv=10
+    )
+
+    grid_search_svm = GridSearchCV(
+        estimator=Pipeline([
+            ('Count_Vectorizer', CountVectorizer()),
+            ('SVM', SVC())
+        ]),
+        param_grid=param_grid_svm,
+        cv=10
+    )
+
+    # Perform grid search and get the best parameters and score for each pipeline
+    best_params_scores = []
+
+    for grid_search, pipeline_name in zip(
+            [grid_search_naive_bayes_cv, grid_search_naive_bayes_tfidf, grid_search_decision_tree, grid_search_knn, grid_search_svm],
+            ['Naive-Bayes + CountVectorizer', 'Naive-Bayes + TF-IDF', 'Decision_Tree', 'KNN', 'SVM']
+    ):
+        grid_search.fit(data.data, data.target)
+        best_params = grid_search.best_params_
+        best_score = grid_search.best_score_
+        best_params_scores.append((pipeline_name, best_params, best_score))
+
+    # Print the best parameters and scores for each pipeline
+    for pipeline_name, best_params, best_score in best_params_scores:
+        print(f"Best parameters for {pipeline_name} pipeline: {best_params}")
+        print(f"Best cross-validation score for {pipeline_name} pipeline: {best_score}")
+        print()
