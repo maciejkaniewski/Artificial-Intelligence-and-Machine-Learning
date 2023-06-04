eaf43eda25f3299b093289229dfe56a4ed9a5ad2
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2023-06-04 23:56:09 +0200

    Upload save_best_model.py

---
+++ 5-Classifier-Machine-Learning/save_best_model.py
@@ -0,0 +1,21 @@
+import joblib as joblib
+from sklearn.datasets import load_files
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+
+if __name__ == "__main__":
+    data = load_files("data", encoding="latin-1")
+
+    pipeline = Pipeline([
+        ('TF-IDF', TfidfVectorizer(ngram_range=(1, 2), stop_words='english')),
+        ('SVM', SVC(C=25, gamma='scale', kernel='sigmoid'))
+    ])
+
+    scores = cross_val_score(pipeline, data.data, data.target, cv=10, n_jobs=-1)
+    mean_accuracy = scores.mean()
+    print(f"Mean Accuracy: {mean_accuracy}")
+
+    pipeline.fit(data.data, data.target)
+    joblib.dump(pipeline, 'trained_model.pkl')
